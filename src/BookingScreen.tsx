642b8699d90ba3a92f5ea488b25dbe7798a4f3ff
geraldkachi <fitzgeraldkachi@gmail.com>
2024-05-12 10:41:01 +0100

    git push -u origin main

--- src/BookingScreen.tsx
+++ src/BookingScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {View} from 'react-native';
 import AddAppointment from './AddPoinitments';
 
-const BookingScreen = ({navigation}) => {
+const BookingScreen = ({navigation}: any) => {
   return (
     <View>
       <AddAppointment {...{navigation}} />
